632eadf8b33aef438010f8dc61ad198e970a29b8
Naveen Reddy <nk0379az20401@outlook.com>
2021-04-28 11:22:55 +0530

    Added Project1 and Project2 work to repo

---
+++ Project2/NeighborlyAPP/NeighborlyAPI/getAdvertisements/__init__.py
@@ -0,0 +1,24 @@
+import azure.functions as func
+import pymongo
+import json
+from bson.json_util import dumps
+import os
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    try:
+        url = os.environ["nk1240DBConnection"]  # TODO: Update with appropriate MongoDB connection information
+        client = pymongo.MongoClient(url)
+        database = client['nk1240databaseproject2']
+        collection = database['nk1240advertisements']
+
+
+        result = collection.find({})
+        result = dumps(result)
+
+        return func.HttpResponse(result, mimetype="application/json", charset='utf-8')
+    except:
+        print("could not connect to mongodb")
+        return func.HttpResponse("could not connect to mongodb",
+                                 status_code=400)
+
